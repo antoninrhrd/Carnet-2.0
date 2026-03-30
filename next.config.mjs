70e37f1303d7dd576a571a42e4150c71df7a3826
antoninrhrd <antoninrhrd@outlook.com>
2026-03-31 01:08:43 +0200

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -4,8 +4,11 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: '*.supabase.co',
-        pathname: '/storage/v1/object/public/**',
+        hostname: '**',
+      },
+      {
+        protocol: 'http',
+        hostname: '**',
       },
     ],
   },
